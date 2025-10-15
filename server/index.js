274a01757fb3324f0a212e77d7fabc3e120c35a7
BUKA34 <kafkass1923@gmail.com>
2025-10-16 00:47:03 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
-const express = require("express"); 
-const app = express(); 
-const http = require("http").Server(app); 
-const io = require("socket.io")(http, { cors: { origin: "*" } }); 
-const PORT = 3001; 
-let users = {}; 
- 
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, { cors: { origin: "*" } });
+const PORT = 3001;
+
+let users = {};
+
 io.on("connection", (socket) => {
   console.log("Yeni bağlantı:", socket.id);
+
+  socket.on("join-room", (room, username) => {
+    socket.join(room);
+    users[socket.id] = { username, room };
+    io.to(room).emit("user-joined", username);
+  });
+
+  socket.on("disconnect", () => {
+    const user = users[socket.id];
+    if (user) io.to(user.room).emit("user-left", user.username);
+    delete users[socket.id];
+  });
+
+  socket.on("signal", (data) => {
+    io.to(data.to).emit("signal", { from: socket.id, signal: data.signal });
+  });
 });
 
-  socket.on("join-room", (room, username) =
-    socket.join(room); 
-    users[socket.id] = { username, room }; 
-    io.to(room).emit("user-joined", username); 
-  }); 
-  socket.on("disconnect", () =
-    const user = users[socket.id]; 
-    if (user) io.to(user.room).emit("user-left", user.username); 
-    delete users[socket.id]; 
-  }); 
-  socket.on("signal", data =
-    io.to(data.to).emit("signal", { from: socket.id, signal: data.signal }); 
-  }); 
-}); 
-http.listen(PORT, () =, PORT)); 
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
