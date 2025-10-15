abcc7af156d5d86d9b8574ba1116f9d8ab133861
BUKA34 <kafkass1923@gmail.com>
2025-10-15 15:38:31 +0300

    App.js

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ 
+      backgroundColor: "#0d1117", 
+      color: "#ffffff", 
+      height: "100vh", 
+      display: "flex", 
+      alignItems: "center", 
+      justifyContent: "center", 
+      flexDirection: "column"
+    }}>
+      <h1>🔥 Bukcord Connected</h1>
+      <p>Welcome to the dark mode interface.</p>
+    </div>
+  );
+}
+
+export default App;
