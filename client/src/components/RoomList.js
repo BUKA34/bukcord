0ef0001c73c098f1ce2b7672843838531e70ab28
BUKA34 <kafkass1923@gmail.com>
2025-10-16 15:15:01 +0300

    RoomList.js

--- client/src/components/RoomList.js
+++ client/src/components/RoomList.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-export default function RoomList({ rooms, currentRoom, onSelect }) {
+export default function RoomList({ rooms, currentRoom, usersByRoom, onSelect }) {
   return (
     <aside className="room-list">
-      <h3>Odalar</h3>
+      <h3>ODALAR</h3>
       <ul>
-        {rooms.map(r => (
+        {rooms.map((r) => (
           <li key={r} className={r === currentRoom ? "active" : ""} onClick={() => onSelect(r)}>
-            {r}
+            <div className="room-title">{r}</div>
+            <div className="room-users">
+              {(usersByRoom[r] || []).slice(0, 6).map(u => (
+                <div key={u.id} className="room-user-dot">
+                  <span className="dot" data-speaking="false" id={`dot-${u.id}`}></span>
+                </div>
+              ))}
+            </div>
+            <div className="room-usernames">
+              {(usersByRoom[r] || []).map(u => (
+                <div key={u.id} className="room-username">{u.username}</div>
+              ))}
+            </div>
           </li>
         ))}
       </ul>
