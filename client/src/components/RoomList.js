66190cbb0761dd3ea3ae5ce131cb3d884feb4a67
BUKA34 <kafkass1923@gmail.com>
2025-10-16 16:08:14 +0300

    RoomList.js

--- client/src/components/RoomList.js
+++ client/src/components/RoomList.js
@@ -1,27 +1,38 @@
 import React from "react";
 
-export default function RoomList({ rooms, currentRoom, usersByRoom, onSelect }) {
+/*
+ props:
+  - rooms: array of room names
+  - currentRoom: selected room name
+  - usersByRoom: { roomName: [ {id, username}, ... ] }
+  - onSelect(room)
+*/
+
+export default function RoomList({ rooms, currentRoom, usersByRoom = {}, onSelect }) {
   return (
     <aside className="room-list">
       <h3>ODALAR</h3>
       <ul>
-        {rooms.map((r) => (
-          <li key={r} className={r === currentRoom ? "active" : ""} onClick={() => onSelect(r)}>
-            <div className="room-title">{r}</div>
-            <div className="room-users">
-              {(usersByRoom[r] || []).slice(0, 6).map(u => (
-                <div key={u.id} className="room-user-dot">
-                  <span className="dot" data-speaking="false" id={`dot-${u.id}`}></span>
-                </div>
-              ))}
-            </div>
-            <div className="room-usernames">
-              {(usersByRoom[r] || []).map(u => (
-                <div key={u.id} className="room-username">{u.username}</div>
-              ))}
-            </div>
-          </li>
-        ))}
+        {rooms.map((r) => {
+          const users = usersByRoom[r] || [];
+          return (
+            <li key={r} className={r === currentRoom ? "active" : ""} onClick={() => onSelect(r)}>
+              <div className="room-title">{r}</div>
+              <div className="room-users">
+                {users.slice(0, 6).map(u => (
+                  <span key={u.id} className="room-user-dot">
+                    <span id={`dot-${u.id}`} className="dot" />
+                  </span>
+                ))}
+              </div>
+              <div className="room-usernames">
+                {users.map(u => (
+                  <div key={u.id} className="room-username">{u.username}</div>
+                ))}
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
